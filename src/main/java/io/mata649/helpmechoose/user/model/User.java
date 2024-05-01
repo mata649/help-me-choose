7b924cf58fab4d083d326b146291338712f99bd5
mata6 <mata649@hotmail.com>
2024-05-01 10:16:49 -0600

    annotations to make my life easier

--- src/main/java/io/mata649/helpmechoose/user/model/User.java
+++ src/main/java/io/mata649/helpmechoose/user/model/User.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +19,9 @@ import java.util.UUID;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User implements UserDetails {
     @Id
     private UUID id;
