33b5152cacd901c106feefdf149c5710831657af
mata6 <mata649@hotmail.com>
2024-05-01 00:19:09 -0600

    User entity and repository added

---
+++ src/main/java/io/mata649/helpmechoose/user/model/User.java
@@ -0,0 +1,54 @@
+package io.mata649.helpmechoose.user.model;
+
+import io.mata649.helpmechoose.role.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+public class User implements UserDetails {
+    @Id
+    private UUID id;
+
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/io/mata649/helpmechoose/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package io.mata649.helpmechoose.user.repository;
+
+
+import io.mata649.helpmechoose.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
