b846e24de3945cfa3c4ace5809319176016998cb
mata6 <mata649@hotmail.com>
2024-04-30 21:22:03 -0600

    Initial security configuration

---
+++ src/main/java/io/mata649/helpmechoose/config/security/HttpSecurityConfig.java
@@ -0,0 +1,21 @@
+package io.mata649.helpmechoose.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class HttpSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authRequestConfig -> authRequestConfig.anyRequest().permitAll()).build();
+    }
+}
