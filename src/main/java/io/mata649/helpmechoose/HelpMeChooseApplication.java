e0b5bdc0196f6f9d3a0696f7556f84e71945d767
mata6 <mata649@hotmail.com>
2024-04-30 21:08:14 -0600

    initial project structure

---
+++ src/main/java/io/mata649/helpmechoose/HelpMeChooseApplication.java
@@ -0,0 +1,13 @@
+package io.mata649.helpmechoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMeChooseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpMeChooseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mata649/helpmechoose/HelpMeChooseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mata649.helpmechoose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpMeChooseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
