7f3960fed14ba43228ad194f4c94cdd6c925ae92
mata6 <mata649@hotmail.com>
2024-05-11 16:30:07 -0600

    authentication controller added

---
+++ src/main/java/io/mata649/helpmechoose/authentication/controller/AuthenticationController.java
@@ -0,0 +1,38 @@
+package io.mata649.helpmechoose.authentication.controller;
+
+import io.mata649.helpmechoose.authentication.application.AuthenticationService;
+import io.mata649.helpmechoose.authentication.application.dto.LoginUserRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.mata649.helpmechoose.authentication.application.dto.AuthenticatedUserResponse;
+import io.mata649.helpmechoose.authentication.application.dto.RegisterUserRequest;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticatedUserResponse> register(@RequestBody RegisterUserRequest request) {
+        AuthenticatedUserResponse response = authenticationService.register(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticatedUserResponse> login(@RequestBody LoginUserRequest request) {
+        AuthenticatedUserResponse response = authenticationService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
