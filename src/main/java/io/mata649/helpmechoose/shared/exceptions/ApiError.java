3f65b258ecdd3f4f8a1a540557053210f4d07c88
mata6 <mata649@hotmail.com>
2024-05-15 21:12:16 -0600

    make ApiError more flexible

--- src/main/java/io/mata649/helpmechoose/shared/exceptions/ApiError.java
+++ src/main/java/io/mata649/helpmechoose/shared/exceptions/ApiError.java
@@ -1,4 +1,17 @@
 package io.mata649.helpmechoose.shared.exceptions;
 
-public record ApiError(String message, Integer statusCode, String path, String method) {
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.context.request.ServletWebRequest;
+
+@Builder
+public record ApiError(Object message, Integer statusCode, String path, String method) {
+    public static ApiError buildBasicError(RuntimeException exc, ServletWebRequest request, HttpStatus status) {
+        return ApiError.builder()
+                .message(exc.getMessage())
+                .statusCode(status.value())
+                .path(request.getRequest().getRequestURL().toString())
+                .method(request.getRequest().getMethod())
+                .build();
+    }
 }
