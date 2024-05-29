ad278e050afd086616f83841e3272606a2e5f55e
Jose Mata <mata649@hotmail.com>
2024-05-29 17:33:25 -0600

    fix add null validation

--- src/main/java/io/mata649/helpmechoose/authentication/application/dto/LoginUserRequest.java
+++ src/main/java/io/mata649/helpmechoose/authentication/application/dto/LoginUserRequest.java
@@ -1,11 +1,12 @@
 package io.mata649.helpmechoose.authentication.application.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record LoginUserRequest(
-        @Size(min = 1, max = 40)
+        @NotNull @Size(min = 1, max = 40)
         String username,
-        @Size(min = 1, max = 64)
+        @NotNull @Size(min = 1, max = 64)
         String password
 ) {
 }
--- src/main/java/io/mata649/helpmechoose/authentication/application/dto/RegisterUserRequest.java
+++ src/main/java/io/mata649/helpmechoose/authentication/application/dto/RegisterUserRequest.java
@@ -1,13 +1,15 @@
 package io.mata649.helpmechoose.authentication.application.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record RegisterUserRequest(
-        @Size(min = 1, max = 40)
+
+        @NotNull @Size(min = 1, max = 40)
         String username,
-        @Size(min = 8, max = 64)
+        @NotNull @Size(min = 8, max = 64)
         String password,
-        @Size(min = 1, max = 64)
+        @NotNull @Size(min = 1, max = 64)
         String repeatedPassword
 ) {
 }
