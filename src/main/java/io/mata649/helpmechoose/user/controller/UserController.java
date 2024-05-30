7508a2c2e9d9a5418f476d9bc896209136bf073d
Jose Mata <mata649@hotmail.com>
2024-05-29 18:13:21 -0600

    add changePassword and update methods

--- src/main/java/io/mata649/helpmechoose/user/controller/UserController.java
+++ src/main/java/io/mata649/helpmechoose/user/controller/UserController.java
@@ -1,18 +1,26 @@
 package io.mata649.helpmechoose.user.controller;
 
-import io.mata649.helpmechoose.user.application.UserService;
-import io.mata649.helpmechoose.user.application.dto.UserResponse;
+import java.util.UUID;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import io.mata649.helpmechoose.config.security.UserRequestDetails;
+import io.mata649.helpmechoose.user.application.UserService;
+import io.mata649.helpmechoose.user.application.dto.ChangePasswordRequest;
+import io.mata649.helpmechoose.user.application.dto.UpdateUserRequest;
+import io.mata649.helpmechoose.user.application.dto.UserResponse;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
@@ -35,6 +43,30 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> findByID(@PathVariable("id") UUID id) {
         UserResponse response = userService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMINISTRATOR')")
+    @PutMapping("/{id}/changePassword")
+    public ResponseEntity<UserResponse> changePassword(@PathVariable("id") UUID id,
+            @RequestBody @Valid ChangePasswordRequest request) {
+        UserRequestDetails currentUser = (UserRequestDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getDetails();
+        request.setCurrentUserID(currentUser.currentID());
+        request.setCurrentUserRole(currentUser.currentRole());
+        UserResponse response = userService.changePassword(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMINISTRATOR')")
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable("id") UUID id,
+            @RequestBody @Valid UpdateUserRequest request) {
+        UserRequestDetails currentUser = (UserRequestDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getDetails();
+        request.setCurrentUserRole(currentUser.currentRole());
+        request.setCurrentUserID(currentUser.currentID());
+        UserResponse response = userService.update(request);
         return ResponseEntity.ok(response);
     }
 }
